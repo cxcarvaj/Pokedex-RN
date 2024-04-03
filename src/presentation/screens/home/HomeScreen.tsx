aff9ce06c59cdf1eff233462f9496e8b0e9815d8
Carlos Carvajal <cxcarvaj@gmail.com>
2024-04-03 12:42:48 -0500

    Implementing infinite scroll

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
-import {useQuery} from '@tanstack/react-query';
+import {useInfiniteQuery} from '@tanstack/react-query';
 import {Text} from 'react-native-paper';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
@@ -13,9 +13,24 @@ import {PokemonCard} from '../../components/pokemons/PokemonCard';
 export const HomeScreen = () => {
   const {top} = useSafeAreaInsets();
 
-  const {isLoading, data: pokemons} = useQuery({
-    queryKey: ['pokemon'],
-    queryFn: () => getPokemons(0),
+  //* This is the regular way to use the useQuery hook
+
+  // const {isLoading, data: pokemons = []} = useQuery({
+  //   queryKey: ['pokemon'],
+  //   queryFn: () => getPokemons(0),
+  //   staleTime: 1000 * 60 * 60, //60 min
+  // });
+
+  const {isLoading, data, fetchNextPage} = useInfiniteQuery({
+    queryKey: ['pokemon', 'infinity'],
+    initialPageParam: 0,
+    queryFn: ({pageParam = 0}) => getPokemons(pageParam),
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length < 20) {
+        return undefined;
+      }
+      return allPages.length;
+    },
     staleTime: 1000 * 60 * 60, //60 min
   });
 
@@ -23,12 +38,15 @@ export const HomeScreen = () => {
     <View style={globalTheme.globalMargin}>
       <PokeBallBg style={styles.imgPosition} />
       <FlatList
-        data={pokemons}
+        data={data?.pages.flat() ?? []}
         keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
         numColumns={2}
         style={{marginTop: top}}
         ListHeaderComponent={() => <Text variant="displayMedium">Pokédex</Text>}
         renderItem={({item: pokemon}) => <PokemonCard pokemon={pokemon} />}
+        onEndReachedThreshold={0.6}
+        onEndReached={() => fetchNextPage()}
+        showsVerticalScrollIndicator={false}
       />
     </View>
   );
